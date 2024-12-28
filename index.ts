20e30096072f6cf527e2f13fb1048a5a52a26f1e
Atharva Srivastava <65837319+atharva162@users.noreply.github.com>
2024-12-28 11:19:42 +0530

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+console.log('Shree Krishna 🙏')
+
+//Implicit types
+
+let hello = "Hello String";
+hello = "23";
+
+let arr = ["jghg"];
+arr = ["test"];
+
+let testObj = {"75": 2};
+testObj = {"75": 343}
+
+
+//Explicit types
+let firstName:String = "Jack";
+let age:Number = 2;
+let isBoy:Boolean = true;
+//Arrays
+let hobbies:Array<String> = ["Reading", "cooking"];
+let newHobbies:String[] = ["Reading", "cooking"];
+let unknownFactAboutJack: any = 767; // can be reassigned to any type of value
